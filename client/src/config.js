ee825d59f39ff5b897684a564db162ddfd5f0628
Dilshan <Dilshan.desilva@infor.com>
2025-09-18 23:28:46 +0530

    Force production API URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://rubber-glue-api.onrender.com' 
-  : 'http://localhost:5000';
+export const API_URL = 'https://rubber-glue-api.onrender.com';
